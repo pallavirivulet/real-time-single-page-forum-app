aa6df4da548f64eec4986f42e6a27312fd106aba
pallavi <pallavi@rivuletdigital.com>
2021-09-22 18:13:07 -0700

    initial commite

---
+++ database/factories/LikeFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\like;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class LikeFactory extends Factory
+{
+    protected $model = like::class;
+
+    public function definition(): array
+    {
+    	return [
+    	    //
+    	];
+    }
+}
